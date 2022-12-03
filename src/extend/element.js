32026fad9a6714844da28baf1e64915516e411fe
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-12-04 01:16:02 +0800

    fix :-1:

--- src/extend/element.js
+++ src/extend/element.js
@@ -3,10 +3,11 @@
  * @Author: maggot-code
  * @Date: 2022-11-21 14:54:00
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-12-04 00:25:26
+ * @LastEditTime: 2022-12-04 01:15:23
  * @Description: 
  */
 import ElementUI from "element-ui";
+import 'element-ui/lib/theme-chalk/index.css';
 
 export function extendElement(Vue) {
     Vue.use(ElementUI);
