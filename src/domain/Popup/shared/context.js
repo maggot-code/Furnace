26ddea6bca5bb4835825f15e37424effa24493aa
maggot_code <wowhuanhuan233@163.com>
2022-12-06 18:13:18 +0800

    fix :-1:

--- src/domain/Popup/shared/context.js
+++
@@ -1,9 +0,0 @@
-/*
- * @FilePath: \Furnace\src\domain\Popup\shared\context.js
- * @Author: maggot-code
- * @Date: 2022-12-05 14:10:46
- * @LastEditors: maggot-code
- * @LastEditTime: 2022-12-05 15:29:03
- * @Description: 
- */
-export const POPUP_SYMBOLE = Symbol('furnace-popup');
