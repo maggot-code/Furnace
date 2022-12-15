a96f531df091844a07f3c765d89d49c8971e6815
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-12-15 13:50:29 +0800

    fix :-1:

--- src/server/router/get/mock.js
+++ src/server/router/get/mock.js
@@ -1,13 +1,13 @@
 /*
- * @FilePath: \Furnace\src\server\router\get\mock.js
+ * @FilePath: /Furnace/src/server/router/get/mock.js
  * @Author: maggot-code
  * @Date: 2022-12-04 05:25:52
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-12-13 17:55:04
+ * @LastEditTime: 2022-12-15 13:26:57
  * @Description: 
  */
-import Response from "./response.json";
-// import Response from "./response.module.json";
+// import Response from "./response.json";
+import Response from "./response.module.json";
 import { defineMock, defineResponse } from "~/mock";
 import { Address, Method } from "./config";
 
